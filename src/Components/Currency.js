3388fcab979de2019c58ac4bcd5c04ac783947b9
aniledev <elinamcgill@gmail.com>
2021-01-09 16:54:20 -0500

    FEAT: change currency style

--- src/Components/Currency.js
+++ src/Components/Currency.js
@@ -4,7 +4,7 @@ export default class Currency extends Component {
   render() {
     const money = new Intl.NumberFormat(this.props.locale, {
       style: "currency",
-      currency: this.props.country,
+      currency: this.props.currency,
     }).format(this.props.value);
 
     return <div className="Currency">{money}</div>;
