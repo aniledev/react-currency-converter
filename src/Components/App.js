4bf920e2b249ffb01e3d227abef45145b6a40cae
aniledev <elinamcgill@gmail.com>
2021-01-09 16:44:47 -0500

    FEAT: import Currency component to App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import "../Styles/App.css";
+import Currency from "../Components/Currency";
 
 export default class App extends Component {
   render() {
-    return <div className="App">Error Boundary App</div>;
+    return (
+      <div className="App">
+        Germany: <Currency value={21} locale="de-DE" currency="USD" />
+        USA: <Currency value={21} locale="en-US" currency="USD" />
+      </div>
+    );
   }
 }
