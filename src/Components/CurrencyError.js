5c39c1f5c3ab8a998db6f4c88031f7af3d96920e
aniledev <elinamcgill@gmail.com>
2021-01-09 17:26:52 -0500

    FEAT: construct method static getDerivedStateFromError to catch all errors

--- src/Components/CurrencyError.js
+++ src/Components/CurrencyError.js
@@ -8,7 +8,17 @@ export default class CurrencyError extends Component {
       hasError: false,
     });
   }
+
+  // This lifecycle is invoked after an error has been thrown by a descendant component. It receives the error that was thrown as a parameter and should return a value to update state.
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
   render() {
-    return <div className="CurrencyError"></div>;
+    // this will conditionally render an error message if hasError is true; otherwise display the children as normal
+    if (this.state.hasError) {
+      return <h2>Could not display this currency.</h2>;
+    }
+    return this.props.children;
   }
 }
